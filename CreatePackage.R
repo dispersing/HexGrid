27fcf339c1703e87b0835e8d62ce9bb995f08394
Christopher Moore <life.dispersing@gmail.com>
2016-05-02 22:24:40 -0400

    Hex grid.coords, etc.

--- CreatePackage.R
+++ CreatePackage.R
@@ -4,4 +4,4 @@ library(roxygen2)
 pkg.dir <- "~/Dropbox/Projects/HexGrid_Package"
 setwd(pkg.dir)
 # create("HexGrid")
-document(paste(pkg.dir, "/HexGrid", sep = ""))
+# document(paste(pkg.dir, "/HexGrid", sep = ""))
